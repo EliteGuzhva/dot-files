b7433021b94dde8f7737aab64b0658afecf2a95d
Гужва Никита <n.guzhva@cognitivepilot.com>
2022-04-28 18:39:12 +0300

    [upd] From workstation

--- .config/herbstluftwm/bash/autostart.sh
+++ .config/herbstluftwm/bash/autostart.sh
@@ -180,14 +180,12 @@ hc set mouse_recenter_gap 0
 
 # rules
 hc unrule -F
-#hc rule class=XTerm tag=3 # move all xterms to tag 3
+hc floating 'Free' on
 hc rule focus=on # normally focus new clients
-#hc rule focus=off # normally do not focus new clients
-# give focus to most common terminals
-#hc rule class~'(.*[Rr]xvt.*|.*[Tt]erm|Konsole)' focus=on
 hc rule windowtype~'_NET_WM_WINDOW_TYPE_(DIALOG|UTILITY|SPLASH)' pseudotile=on
 hc rule windowtype='_NET_WM_WINDOW_TYPE_DIALOG' focus=on
 hc rule windowtype~'_NET_WM_WINDOW_TYPE_(NOTIFICATION|DOCK|DESKTOP)' manage=off
+hc rule class='Workrave' pseudotile=on
 
 # unlock, just to be sure
 hc unlock
